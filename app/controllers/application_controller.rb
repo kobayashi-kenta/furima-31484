9dcd8cfef13398b6259b755a5bc3d48a1f585200
kobayashi-kenta <12321klj@gmail.com>
2020-10-31 12:50:02 +0900

    ユーザーログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name_1, :first_name_1, :last_name_2, :first_name_2, :birthday])
+  end
 end
