87fc8cb2151c20ce2003155dbbdb7bafbebd9a98
kobayashi-kenta <12321klj@gmail.com>
2020-11-01 20:08:54 +0900

    validationの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :password, length: {minimum: 6}
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX },length: {minimum: 6}
   validates :last_name_1, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナ、漢字のみで入力して下さい"}
   validates :first_name_1, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナ、漢字のみで入力して下さい"}
   validates :last_name_2, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナのみで入力して下さい"}
