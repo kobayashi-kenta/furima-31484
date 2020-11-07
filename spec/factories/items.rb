83f0a84b3d4700d563cb7db5db742d478d8dffa5
kobayashi-kenta <12321klj@gmail.com>
2020-11-07 15:10:20 +0900

    テストコードのためのダミーデータの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name {'aaa'}
+    description {'abcdefghijklmn'}
+    area_id {2}
+    category_id {2}
+    money_type_id {2}
+    send_day_id {2}
+    status_id {2}
+    price {4000}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
