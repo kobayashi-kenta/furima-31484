9236b49430acb7c8052986dfead974ce51a986e6
kobayashi-kenta <12321klj@gmail.com>
2020-11-07 17:19:00 +0900

    JSファイルの用意

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../sale_fee")
+require("../sale_profit")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
