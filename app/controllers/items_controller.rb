14768ead2e1b9a9eee67c3ec324a1df29286513d
kobayashi-kenta <12321klj@gmail.com>
2020-11-08 17:57:42 +0900

    edit,updateアクション＆ユーザーによる条件分岐の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :find_item, only: [:show]
+  before_action :find_item, only: [:show, :edit]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -22,6 +24,20 @@ class ItemsController < ApplicationController
 
   end
 
+  def edit
+
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+
+  end
+
   private
 
   def item_params
@@ -31,4 +47,12 @@ class ItemsController < ApplicationController
   def find_item
     @item = Item.find(params[:id])
   end
+
+  def move_to_index
+    @user = User.find(params[:id])
+    unless user_signed_in? && current_user.id == @user.id
+      redirect_to action: :index
+    end
+  end
+
 end
