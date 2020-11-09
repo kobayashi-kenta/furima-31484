32ac76693dd3aae0bf0dd457d701ca2eccb02ee3
kobayashi-kenta <12321klj@gmail.com>
2020-11-09 20:46:51 +0900

    valisationの追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :send_user
+
+  validates :user_id, presence: true
+  validates :item_id, presence: true
 end
--- app/models/send_user.rb
+++ app/models/send_user.rb
@@ -1,3 +1,14 @@
 class SendUser < ApplicationRecord
   belongs_to :order
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+
+  with_options presence: true do
+    validates :item_id
+    validates :area_id
+    validates :adress_id
+    validates :city
+    validates :house_number
+    validates :phone_number
+  end
 end
