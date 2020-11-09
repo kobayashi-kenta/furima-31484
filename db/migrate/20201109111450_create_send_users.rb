335221509f99678a6d7a119d55586cf18dffedef
kobayashi-kenta <12321klj@gmail.com>
2020-11-09 20:24:37 +0900

    購入者、購入情報に関するvalidationの追加

---
+++ db/migrate/20201109111450_create_send_users.rb
@@ -0,0 +1,14 @@
+class CreateSendUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :send_users do |t|
+      t.references :item, null: false, foreign_key: true
+      t.integer :area_id, null: false
+      t.string :adress_number, null: false
+      t.string :city, null: false
+      t.string :house_number, null: false
+      t.string :building_name
+      t.string :phone_number, null: false
+      t.timestamps
+    end
+  end
+end
