3cb524c807bbe1f2442978cd309debf59ab1ae13
kobayashi-kenta <12321klj@gmail.com>
2020-11-10 20:52:49 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
 
   def index
     @order_info = OrderInfo.new
-    redirect_to root_path if current_user.id == @item.user_id || @item.order != nil
+    redirect_to root_path if !user_signed_in? || current_user.id == @item.user_id || @item.order.item_id != nil 
   end
 
 
