80029d1354a014f2dfcb315383ea66234a6bcde2
kobayashi-kenta <12321klj@gmail.com>
2020-11-10 20:26:35 +0900

    コントローラーに対する、カード機能の追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,40 @@
 class OrdersController < ApplicationController
+  before_action :order_create, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @order_info = OrderInfo.new
+    redirect_to root_path if current_user.id == @item.user_id || @item.order != nil
   end
 
+
   def create
-    @item = Item.find(params[:item_id])
     @order_info = OrderInfo.new(order_params)
     if @order_info.valid?
+      pay_item
       @order_info.save
       return redirect_to root_path
     else
       render 'index'
     end
   end
-
+  
   private
+  
+  def order_create
+    @item = Item.find(params[:item_id])
+  end
 
   def order_params
-    params.require(:order_info).permit(:area_id,:adress_number,:city,:building_name,:house_number,:phone_number).merge(user_id: current_user.id,item_id: @item.id)
+    params.require(:order_info).permit(:area_id,:adress_number,:city,:building_name,:house_number,:phone_number).merge(user_id: current_user.id,item_id: @item.id,token: params[:token])
   end
+
+  def pay_item
+  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+  Payjp::Charge.create(
+    amount: @item.price,  # 商品の値段
+    card: order_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+  )
+  end
+
 end
