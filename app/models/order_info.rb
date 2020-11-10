6b5c97f7e0f2a174f6e8394b36b1ed86bba6721b
kobayashi-kenta <12321klj@gmail.com>
2020-11-10 19:57:14 +0900

    tokenのvalidationの追加

--- app/models/order_info.rb
+++ app/models/order_info.rb
@@ -1,16 +1,17 @@
 class OrderInfo
   include ActiveModel::Model
-  attr_accessor :item_id,:user_id,:area_id,:adress_number,:city,:building_name,:house_number,:phone_number
+  attr_accessor :item_id,:user_id,:area_id,:adress_number,:city,:building_name,:house_number,:phone_number,:token
 
   # ここにバリデーションの処理を書く
   with_options presence: true do
     validates :item_id
-    validates :area_id
+    validates :area_id, numericality: { other_than: 1 }
     validates :adress_number,format: {with: /\A\d{3}[-]\d{4}\z/ , message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :house_number
     validates :phone_number,format: {with: /\A\d{10,11}\z/ , message: "is invalid."}
     validates :user_id
+    validates :token
   end
 
   def save
